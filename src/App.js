73862e62d627f2ff575e3e247213198e56224060
Vesile <vesile_emre@hotmail.com>
2023-10-25 13:49:54 +0300

    styling added to the component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
-
 import SiriImage from "./images/siri.png";
 import AlexaImage from "./images/alexa.png";
 import CoronaImage from "./images/cortana.png";
@@ -7,10 +7,29 @@ import CoronaImage from "./images/cortana.png";
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard image={AlexaImage} title="Alexa" handle="@alexa99" />
-            <ProfileCard image={SiriImage} title="Siri" handle="@siri99" />
-            <ProfileCard image={CoronaImage} title="Cortana" handle="@cortana" />
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>
+                </div>
+            </section>
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCard image={AlexaImage} title="Alexa" handle="@alexa99" />
+
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard image={SiriImage} title="Siri" handle="@siri99" />
+
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard image={CoronaImage} title="Cortana" handle="@cortana" />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
         </div>
     )
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,11 +2,24 @@ function ProfileCard({ title, handle, image }) {
     //const { title, handle } = props;
     // destructuring const title = props.title; const handle = props.handle;
 
-    return (<div>
-        <img src={image} alt="pda logo" />
-        <div>ProfileCard title is : {title}</div>
-        <div>ProfileCard handler is : {handle} </div>
-    </div>);
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="pda logo" />
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4"> {title}</p>
+                    <p className="subtitle is-6"> {handle} </p>
+                </div>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default ProfileCard;
